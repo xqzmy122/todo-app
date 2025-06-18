91fa4be92011f73815a14468fa816cd48642b870
Miron <mironchik515ke@gmail.com>
2025-06-18 13:37:00 +0300

    added post method to update todo list

--- server/index.js
+++ server/index.js
@@ -5,9 +5,15 @@ const app = express()
 const PORT = 3000
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/todos', (req, res) => {
   res.send(todos)
+})
+
+app.post('/todos', (req, res) => {
+  todos.push(req.body)
+  res.send('200')
 })
 
 app.listen(PORT, () => {
